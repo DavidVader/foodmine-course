e30fd77f913f4d9c9aa069128aaf0b9591c5612c
DavidVader <155568063+DavidVader@users.noreply.github.com>
2024-11-23 14:54:53 +0100

    Part 7 - Tags Bar

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Food } from '../../../shared/models/Food';
 import { CurrencyPipe } from '@angular/common';
 import { SearchComponent } from "../../partials/search/search.component";
+import { TagsComponent } from "../../partials/tags/tags.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CurrencyPipe, SearchComponent],
+  imports: [RouterLink, CurrencyPipe, SearchComponent, TagsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -17,11 +18,12 @@ export class HomeComponent {
 
   constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
     activatedRoute.params.subscribe((params) => {
-      if (params.searchTerm) {
+      if (params.searchTerm)
         this.foods = foodService.getAllFoodsBySearchTerm(params.searchTerm);
-      } else {
+      else if (params.tag)
+        this.foods = foodService.getAllFoodsByTag(params.tag);
+      else
         this.foods = foodService.getAll();
-      }
     })
   }
 }
